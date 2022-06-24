c6e0c6bfed001ec2a1e776e07c2b2fb88ccff969
a631807682 <631807682@qq.com>
2022-06-24 12:16:07 +0800

    test: add more tests

--- tests/ddltransform_test.go
+++ tests/ddltransform_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"github.com/a631807682/ddltransform"
+	"github.com/a631807682/ddltransform/schema"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTransform(t *testing.T) {
+func TestDefaultTransform(t *testing.T) {
 	ddl := `
 	CREATE TABLE blacklists  (
 		id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -26,4 +27,43 @@ func TestTransform(t *testing.T) {
 	})
 	assert.Equal(t, nil, err)
 	assert.Equal(t, expectCode, code)
+}
+
+type emptyTransformer struct {
+}
+
+func (*emptyTransformer) Name() string {
+	return "empty_transformer"
+}
+
+func (*emptyTransformer) Transform(table string, fields []schema.Field) (string, error) {
+	return "empty", nil
+}
+func TestCustomizeTransform(t *testing.T) {
+	ddl := `
+	CREATE TABLE blacklists  (
+		id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+		PRIMARY KEY (id) USING BTREE
+	) ENGINE = InnoDB AUTO_INCREMENT = 1;
+	`
+	code, err := ddltransform.Transform(ddl, ddltransform.Config{
+		ParserType:  ddltransform.Mysql,
+		Transformer: &emptyTransformer{},
+	})
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "empty", code)
+}
+
+func TestError(t *testing.T) {
+	var err error
+	_, err = ddltransform.Transform("", ddltransform.Config{
+		ParserType: ddltransform.Mysql,
+	})
+	assert.NotEqual(t, nil, err)
+
+	_, err = ddltransform.Transform("", ddltransform.Config{
+		TransformerType: ddltransform.Gorm,
+	})
+	assert.NotEqual(t, nil, err)
 }
